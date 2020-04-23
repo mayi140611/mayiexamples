393352a6b7168d05bf2e4fb9387d8f4dade0eee7
ian <yongguiluo@hotmail.com>
2020-04-23 14:03:50 +0800

    sm

--- RecommendatIon_System/preprocess.py
+++ RecommendatIon_System/preprocess.py
@@ -1,10 +1,12 @@
 import random
 import numpy as np
 from tqdm import tqdm
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 
-def gen_data_set(data, negsample=0):
-
+def gen_data_set(data, negsample: int=0):
+    """
+    negsample: 负样本数相对于正样本数的倍数
+    """
     data.sort_values("timestamp", inplace=True)
     item_ids = data['movie_id'].unique()
 
@@ -15,6 +17,7 @@ def gen_data_set(data, negsample=0):
         rating_list = hist['rating'].tolist()
 
         if negsample > 0:
+            # 产生负样本，策略：从用户没有评分的items中随机有放回采样
             candidate_set = list(set(item_ids) - set(pos_list))
             neg_list = np.random.choice(candidate_set,size=len(pos_list)*negsample,replace=True)
         for i in range(1, len(pos_list)):
@@ -41,7 +44,7 @@ def gen_model_input(train_set,user_profile,seq_max_len):
     train_label = np.array([line[3] for line in train_set])
     train_hist_len = np.array([line[4] for line in train_set])
 
-    train_seq_pad = pad_sequences(train_seq, maxlen=seq_max_len, padding='post', value=0)
+    train_seq_pad = pad_sequences(train_seq, maxlen=seq_max_len, padding='post', truncating='post', value=0)
     train_model_input = {"user_id": train_uid, "movie_id": train_iid, "hist_movie_id": train_seq_pad,
                          "hist_len": train_hist_len}
 
